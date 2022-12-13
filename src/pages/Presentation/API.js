f542c28c1b285969912c5d60d7afbe21b0b24e6b
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2022-12-13 21:55:06 +0700

    add file api

---
+++ src/pages/Presentation/API.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { ApiConfig as _ParamConfig } from "../../actions/constants";
+
+let AxiosInstance = axios.create({
+  baseURL: _ParamConfig.serverUrl,
+  timeout: _ParamConfig.timeout
+});
+
+// #region private methods
+
+// const PostAsync = async (method, requestData) => {
+//   var response = await AxiosInstance.post(method, requestData, {
+//     headers: {
+//       x_authorization: localStorage.getItem("accessToken")
+//     }
+//   });
+//   return response.data;
+// };
+
+// #endregion
+
+// #region Main methods
+
+export const GetAllPresentations = async () => {
+  var accessToken = localStorage.getItem("accessToken");
+  console.log("accessToken:", accessToken);
+  var response = await AxiosInstance.get("presentation", null, {
+    headers: {
+      x_authorization: accessToken
+    }
+  });
+  console.log(response);
+  console.log(response);
+  return response;
+};
+
+export const AddPresentation = async (request) => {
+  var requestData = {
+    name: request.presentationName
+  };
+  var response = await AxiosInstance.post("presentation", requestData, {
+    headers: {
+      x_authorization: localStorage.getItem("accessToken")
+    }
+  });
+  console.log(response);
+  return response.data;
+};
+// #endregion
