953a955d9b2099b9c40b02afbae898236e298c61
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2022-12-14 17:06:08 +0700

    update item: show slide (first time)

--- src/pages/Presentation/Show/style.jsx
+++ src/pages/Presentation/Show/style.jsx
@@ -1,9 +1,28 @@
 import styled from "styled-components";
-const Styled = styled.div`
+import { Menu } from "antd";
+
+export const Styled = styled.div`
   .show_presentation-container {
     display: flex;
     align-items: center;
     justify-content: center;
   }
 `;
-export default Styled;
+
+export const StyleMenu = styled(Menu)`
+  width: fit-content;
+  max-width: 4rem;
+  display: inline;
+  z-index: 2;
+  position: absolute;
+  bottom: -25rem;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(-90deg);
+  transition: bottom 0.1s linear;
+  border-radius: 0.4rem;
+  &:hover {
+    bottom: -22rem;
+  }
+`;
+
+export const StyledOption = styled.div``;
--- src/pages/Presentation/Slide.jsx
+++ src/pages/Presentation/Slide.jsx
@@ -1,13 +1,19 @@
 import * as React from "react";
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from "recharts";
+
 
 export const Slide = (props) => {
-  const { id, type, question, options } = props.slide;
-
+  // const { id, type, question, options } = props.slide;
   return (
     <>
-      <h2>
-        {id} - {type} - {question}
-      </h2>
+      <BarChart width={430} height={500} data={props.dataChart}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="answer" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="total" fill="#8884d8" />
+      </BarChart>
     </>
   );
 };
