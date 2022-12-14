fff2693b9b2898be946f884fc72e2d6aa52a38dc
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2022-12-14 11:47:18 +0700

    update tạo slide default khi tạo presentation

--- src/pages/Presentation/Show/ShowPresentation.jsx
+++ src/pages/Presentation/Show/ShowPresentation.jsx
@@ -1,3 +1,15 @@
 import * as React from "react";
+import PresentationContext from "../../../utils/PresentationContext";
+import { Layout, Divider } from "antd";
+const { Header, Footer, Sider, Content } = Layout;
 
-export const ShowPresentation = props => {};
+export const ShowPresentation = (props) => {
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+  const [isFullScreen, setIsFullScreen] = React.useState(true);
+  const [presentation, setPresentation] = React.useContext(PresentationContext);
+
+  return;
+  <Layout>
+    <Content>{presentation.name}</Content>
+  </Layout>;
+};
